5566e9cdfe9784a0442ebad5f2bdc3151f807d1d
KamilMalicki <146217192+KamilMalicki@users.noreply.github.com>
2023-12-09 14:38:36 +0100

    Add files via upload

--- intlang.py
+++ intlang.py
@@ -10,9 +10,50 @@ def interpretuj(plik):
     RAY = 0
     RAX = 0
     line_number = 0
+    RAR = [0] * 8
 
+    def atoi_func(reg):
+     global RAX, RAY
+     if reg.startswith('$'):
+        reg_name = reg[1:]
+        if reg_name == 'RAX':
+            RAX -= 48
+        elif reg_name == 'RAY':
+            RAY -= 48
+        else:
+            if reg_name.startswith('RAR[') and reg_name.endswith(']'):
+                index = int(reg_name.split('[')[1][:-1])
+                if 0 <= index < 8:
+                    RAR[index] -= 48
+                else:
+                    print("SYNTAX ERROR")
+            else:
+                print("SYNTAX ERROR")
+     else:
+        print("SYNTAX ERROR")
+    
+    def itoa_func(reg):
+     if reg.startswith('$'):
+        reg_name = reg[1:]
+        if reg_name == 'RAX':
+            print(chr(RAX))
+        elif reg_name == 'RAY':
+            print(chr(RAY))
+        else:
+            if reg_name.startswith('RAR[') and reg_name.endswith(']'):
+                index = int(reg_name.split('[')[1][:-1])
+                if 0 <= index < 8:
+                    print(chr(RAR[index]))
+                else:
+                    print("SYNTAX ERROR")
+            else:
+                print("SYNTAX ERROR")
+     else:
+        print("SYNTAX ERROR")
+    
     def print_func(*args):
         nonlocal RAX
+        nonlocal RAY
         for arg in args:
             if arg == '$RAX':
                 print(RAX, end=' ')
@@ -40,7 +81,24 @@ def interpretuj(plik):
         elif instrukcja[0] == 'INC':
             RAX = RAX + 1
             line_number += 1
-            
+
+        elif instrukcja[0] == 'STORE':
+         index = int(instrukcja[1])
+         value = int(instrukcja[2])
+         if 0 <= index < 8:
+          RAR[index] = value
+         else:
+          print("SYNTAX ERROR")
+         line_number += 1
+
+        elif instrukcja[0] == 'DATA':
+           index = int(instrukcja[1])
+           if 0 <= index < 8:
+            print(RAR[index])
+           else:
+            print("SYNTAX ERROR")
+           line_number += 1
+           
         elif instrukcja[0] == 'DOUBLE':
             RAX = RAX * 2
             line_number += 1
@@ -53,10 +111,25 @@ def interpretuj(plik):
              RAX = RAY
              line_number += 1
 
+        elif instrukcja[0] == 'ITOA':
+            if len(instrukcja) == 2:
+                itoa_func(instrukcja[1])
+            else:
+                print("SYNTAX ERROR")
+            line_number += 1
+
+        elif instrukcja[0] == 'ATOI':
+            if len(instrukcja) == 2:
+                atoi_func(instrukcja[1])
+            else:
+                print("SYNTAX ERROR")
+            line_number += 1
+        
         elif instrukcja[0] == 'SUM':
         	RAX = RAX + RAY
         	line_number += 1
         	
+
         elif instrukcja[0] == 'SUB':
         	RAX = RAX - RAY
         	line_number += 1
@@ -77,7 +150,18 @@ def interpretuj(plik):
         	if RAX==warunek and instrukcja[2]=='GOTO':
         		line_number = int(instrukcja[3])
         	elif instrukcja[2]!='GOTO':
-        		print("ERROR")
+        		print("SYNTAX ERROR")
+        	else:
+        		line_number += 1
+
+        		
+        elif instrukcja[0] =='RARCMP':
+        	a=int(instrukcja[1])
+        	b=int(instrukcja[2])
+        	if RAR[a]==RAR[b] and instrukcja[3]=='GOTO':
+        		line_number = int(instrukcja[4])
+        	elif instrukcja[3]!='GOTO':
+        		print("SYNTAX ERROR")
         	else:
         		line_number += 1
         		
@@ -90,8 +174,7 @@ def interpretuj(plik):
         	sys.exit(0)
         
         else:
-        	print("ERROR")
-        	sys.exit(0)
+        	line_number += 1
         	
 if __name__ == "__main__":
     if len(sys.argv) != 2:
